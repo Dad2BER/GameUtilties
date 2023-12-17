66ffe58b6e7b81e9665068469f57b0deb5beda75
Dad2BER <Dad2BER@gmail.com>
2023-12-17 17:07:41 -0600

    Added simpler basic sprites in preperation for map functionality

--- sprite_classes/knownSprites.js
+++ sprite_classes/knownSprites.js
@@ -1,12 +1,16 @@
-import { Sprite } from "./sprite.js";
+import { Sprite, RandomeSpirte, AnimatedSprite } from "./sprite.js";
 
 // Skeleton
-export class skeletonWalkLeft extends Sprite { constructor(x,y) { super('skeleton_Walk_Left',x,y,128,80,8,15, true); } }
-export class skeletonWalkRight extends Sprite { constructor(x,y) { super('skeleton_Walk_Right',x,y,128,80,8,15, true); } }
-export class skeletonAttack1 extends Sprite { constructor(x,y) {super('skeleton_Attack_1',x,y,128,80,7,15, false); } }
-export class skeletonDead extends Sprite { constructor(x,y) {super('skeleton_Dead',x,y,128,80,3,15, true); } }
-export class skeletonHurt extends Sprite { constructor(x,y) {super('skeleton_Hurt',x,y,128,80,3,15, false); } }
-export class skeletonIdle extends Sprite { constructor(x,y) {super('skeleton_Idle',x,y,128,80,7,15, true);} }
-export class skeletonJump extends Sprite { constructor(x,y) {super('skeleton_Jump',x,y,128,80,10,15, false); }  }
-export class skeletonRun extends Sprite { constructor(x,y) {super('skeleton_Run',x,y,128,80,7,15, true);} }
+export class skeletonWalkLeft extends AnimatedSprite { constructor(x,y) { super('skeleton_Walk_Left',x,y,128,80,8,15, true); } }
+export class skeletonWalkRight extends AnimatedSprite { constructor(x,y) { super('skeleton_Walk_Right',x,y,128,80,8,15, true); } }
+export class skeletonAttack1 extends AnimatedSprite { constructor(x,y) {super('skeleton_Attack_1',x,y,128,80,7,15, false); } }
+export class skeletonDead extends AnimatedSprite { constructor(x,y) {super('skeleton_Dead',x,y,128,80,3,15, true); } }
+export class skeletonHurt extends AnimatedSprite { constructor(x,y) {super('skeleton_Hurt',x,y,128,80,3,15, false); } }
+export class skeletonIdle extends AnimatedSprite { constructor(x,y) {super('skeleton_Idle',x,y,128,80,7,15, true);} }
+export class skeletonJump extends AnimatedSprite { constructor(x,y) {super('skeleton_Jump',x,y,128,80,10,15, false); }  }
+export class skeletonRun extends AnimatedSprite { constructor(x,y) {super('skeleton_Run',x,y,128,80,7,15, true);} }
 
+//Dugeon
+export class randomeBrickBrown extends RandomeSpirte { constructor(x,y) {super('brick_brown',x,y,32,32,8)}}
+export class randomGrayFloor extends RandomeSpirte { constructor(x,y) {super('floor_gray',x,y,32,32,4)}}
+export class door extends Sprite { constructor(x,y) {super('door',x,y,32,32,2)}}
--- sprite_classes/sprite.js
+++ sprite_classes/sprite.js
@@ -1,7 +1,9 @@
 export class Sprite {
     
-    constructor(spriteSheetImageID, x, y, spriteWidth, spriteHeight, numberFrames, fps, loop) {
+    constructor(spriteSheetImageID, x, y, spriteWidth, spriteHeight, numberFrames, frameX, frameY) {
         this.image = document.getElementById(spriteSheetImageID); //Image sheet to be used for this sprite
+        this.frameX = frameX; //Which X frame are we currently on
+        this.frameY = frameY;
         this.maxFrames = numberFrames-1;
         this.width = spriteWidth; //Width of one sprite frame
         this.height = spriteHeight; //Height of one sprite frame
@@ -11,18 +13,6 @@ export class Sprite {
         this.drawX = this.x - this.width/2;
         this.drawY = this.y - this.height/2;
 
-        this.frameX = 0; //Which X frame are we currently on
-        this.fps = fps; //How many frames per second should display (how fast is the animation)
-        this.frameInterval = 1000/this.fps; //Based on the fps, we can determin how much time to leave between frames
-        this.frameTimer = 0; //Need to keep track of how long we have been on this frame
-        
-        this.loop = loop;
-        this.animationFinished = false;
-    }
-
-    restartArnimation() {
-        this.frameX = 0;
-        this.animationFinished = false;
     }
 
     move(deltaX, deltaY) {
@@ -40,6 +30,41 @@ export class Sprite {
     }
 
     update(deltaTime) {
+    }
+
+    draw(context, drawHitBox) {
+        if (drawHitBox === true) {
+            context.strokeRect(this.drawX, this.drawY, this.width, this.height);
+        }
+        context.drawImage(this.image, this.frameX * this.width, this.frameY * this.height, this.width, this.height, 
+                          this.drawX, this.drawY, this.width, this.height);
+    }
+}
+
+export class RandomeSpirte extends Sprite {
+    constructor(spriteSheetImageID, x, y, spriteWidth, spriteHeight, numberFrames) {
+        super(spriteSheetImageID, x, y, spriteWidth, spriteHeight, numberFrames, Math.floor(Math.random() * numberFrames),0)
+    }
+}
+
+export class AnimatedSprite extends Sprite {
+    constructor(spriteSheetImageID, x, y, spriteWidth, spriteHeight, numberFrames, fps, loop) {
+        super(spriteSheetImageID, x, y, spriteWidth, spriteHeight, numberFrames, 0, 0);
+        this.fps = fps; //How many frames per second should display (how fast is the animation)
+        this.frameInterval = 1000/this.fps; //Based on the fps, we can determin how much time to leave between frames
+        this.frameTimer = 0; //Need to keep track of how long we have been on this frame
+        
+        this.loop = loop;
+        this.animationFinished = false;
+    }
+
+    restartArnimation() {
+        this.frameX = 0;
+        this.animationFinished = false;
+    }
+
+    update(deltaTime) {
+        super.update(deltaTime);
         //Sprite Animation
         if (this.frameTimer > this.frameInterval) { //If we have spent enough time on this frame, move to the next
             this.frameTimer = 0;
@@ -57,12 +82,7 @@ export class Sprite {
         }
     }
 
-    draw(context, drawHitBox) {
-        if (drawHitBox === true) {
-            context.strokeRect(this.drawX, this.drawY, this.width, this.height);
-        }
-        context.drawImage(this.image, this.frameX * this.width, 0, this.width, this.height, 
-                          this.drawX, this.drawY, this.width, this.height);
-    }
+
+
 }
 
